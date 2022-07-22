57899fdb8d2e3d8a6ee84df3052d1c509304c63b
DESKTOP-ULHO8GS\work <271677538@qq.com>
2022-07-22 17:11:37 +0800

    add

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,7 +3,7 @@ import {parseHeaders} from "./helpers/headers"
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     return new Promise((resolve, reject) => {
-        const {data = null, url, method = 'get', headers = {}, responseType} = config
+        const {data = null, url, method = 'get', headers = {}, responseType, timeout} = config
         // 1.创建XMLHttpRequest异步对象
         const request = new XMLHttpRequest()
         // 2.配置请求参数
@@ -17,11 +17,17 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         if (responseType) {
             request.responseType = responseType;
         }
+        if (timeout) {
+            request.timeout = timeout;
+        }
         // 3.发送请求
         request.send(data)
         // 4.注册事件，拿到响应信息
         request.onreadystatechange = function handleLoad() {
             if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 0) {
                 return;
             }
             const responseHeaders = parseHeaders(request.getAllResponseHeaders());
@@ -37,7 +43,24 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
                 config,
                 request
             };
-            resolve(response);
+
+            function handleResponse(response: AxiosResponse): void {
+                if (response.status >= 200 && response.status < 300) {
+                    resolve(response);
+                } else {
+                    reject(new Error(`Request failed with status code ${response.status}`));
+                }
+            }
+
+            handleResponse(response);
         }
+        // 4.1 网络错误事件
+        request.onerror = function () {
+            reject(new Error("Net Error"));
+        };
+        // 4.2 超时错误事件
+        request.ontimeout = function () {
+            reject(new Error(`Timeout of ${timeout} ms exceeded`));
+        };
     });
 }
